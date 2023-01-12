b9115887c7c6d6b1b10d6e77bbdca20af5f89db4
Kenan Altaki <kenan.altaki@gmail.com>
2023-01-12 21:54:29 +0200

    MVP 1

--- app.py
+++ app.py
@@ -1,9 +1,34 @@
-from flask import Flask
+import asyncio
+import aiohttp
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def social_network_activity():
-    # TODO: your code here
-    json_response = {}
+
+@app.route('/', methods=['GET'])        # Only allow get requests
+async def social_network_activity():    # make async coroutine
+
+    activity = {}
+
+    async with aiohttp.ClientSession() as session:
+        tasks = [
+            fetch(activity, session, 'facebook',
+                  'https://takehome.io/facebook'),
+            fetch(activity, session, 'twitter', 'https://takehome.io/twitter'),
+            fetch(activity, session, 'instagram',
+                  'https://takehome.io/instagram'),
+        ]
+        await asyncio.gather(*tasks)
+
+    json_response = jsonify(activity)
     return json_response
+
+
+async def fetch(activity, session, name, url):
+    async with session.get(url) as response:
+        try:
+            results = await response.json()
+            activity[name] = len(results)
+
+        except Exception as e:
+            print('Exception!', e)
