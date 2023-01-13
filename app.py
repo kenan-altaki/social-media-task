2154dca3f9f9c6840ae8c39cbe2ead126c1d1d28
Kenan Altaki <kenan.altaki@gmail.com>
2023-01-13 09:55:37 +0200

    Updated comments to better describe intent

--- app.py
+++ app.py
@@ -4,34 +4,38 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 
+# =============================================================================
+# # Assumptions made
+# =============================================================================
+#  - Speed > Robustness
+#      - use multi-threading to fetch data.
+#      - use `aiohttp` rather than `requests`
+#      - since the latter is not thread-safe.
+#      - use `asyncio` rather than `threading`.
+#  - Robustness > Correctness
+#      - if incomplete/wrong data is recieved,
+#      - flag it as wrong and return a response
+#      - do not abort request,
+#      - and do not attempt a retry due to speed requirements
+# =============================================================================
+
 # List of API endpoints names and their urls to be queried.
+# This allows us to scale up and down without much effort.
 API_LIST = [
     {'name': 'facebook', 'url': 'https://takehome.io/facebook'},
     {'name': 'twitter', 'url': 'https://takehome.io/twitter'},
     {'name': 'instagram', 'url': 'https://takehome.io/instagram'},
 ]
 
-'''
-# Assumptions made
-
- - Speed > Robustness
-     - use multi-threading to fetch data.
-     - use `aiohttp` rather than `requests`
-     - since the latter is not thread-safe.
-     - use `asyncio` rather than `threading`.
- - Robustness > Correctness
-     - if incomplete/wrong data is recieved, 
-     - flag it as wrong and return a response
-     - do not abort request, 
-     - and do not attempt a retry due to speed requirements
-'''
+# Some options
+FAIL_ON_UNHANDLED_EXCEPTION = True
 
 app = Flask(__name__)
 
 # CORS!
-# needed when client app is not on the same host
-# learned this the hard way!!!
+# Needed when client app is not on the same host. Learned this the hard way!!!
 cors = CORS(app)
+
 
 @app.route('/', methods=['GET'])        # Only allow get requests
 async def social_network_activity():    # make async coroutine
@@ -46,7 +50,7 @@ async def social_network_activity():    # make async coroutine
 
     async def fetch(session, name, url):
         '''The async function that does the work.
-        
+
         Function that fetches the data from APIs defined up top.
         Attempts to parse the body of the response into JSON, and if successful 
         returns the activity level. Otherwise `null` if cannot parse or `-1` 
@@ -58,24 +62,40 @@ async def social_network_activity():    # make async coroutine
                 activity[name] = len(results)
 
             except aiohttp.ContentTypeError as e:
-                # this is the expected Exception, when the GET request does 
-                # not receive a properly formatted JSON
+                # This is the expected Exception, when the GET request does not
+                # receive a properly formatted JSON.
                 print('ContentTypeError!', e)
+                # Set the value of the activity to `null` and continue.
+                # Based on assumption 2 above.
+                # `null` is the correcter way to pass it in JSON.
                 activity[name] = None
-                # `null` is the correcter way to pass it in JSON
 
             except Exception as e:
-                # for all other Exceptions
+                # It is generally a bad idea to catch all exceptions.
+                # Since it eliminates the ability to respond to diferent
+                # errors adequately.
+                #
+                # A better idea is to discuss this with the client and agree
+                # on a suitable way to pass/handle them.
                 print('Exception!', e)
+
+                # Set the value of the activity to `-1`, to indicate a different
+                # type of error to the expected one.This value will be sent to
+                # client if FAIL_ON_UNHANDLED_EXCEPTION = False
                 activity[name] = -1
 
+                # For now, set an option that can be changed if you want to not
+                # carry on on an unhandled exception.
+                if FAIL_ON_UNHANDLED_EXCEPTION:
+                    return '', 500
+
     async with aiohttp.ClientSession() as session:
-        # create the session object in context and generate the 
-        # tasks for `asyncio`.`gather()`
+        # Create the session object in context and generate the tasks.
         tasks = [
             fetch(session, api['name'], api['url'])
             for api in API_LIST
         ]
+
         await asyncio.gather(*tasks)
 
     return jsonify(activity), 200
